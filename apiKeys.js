c4eed1f91382c68bd9d7cc08ccf24700419caaf8
Don Knapp <don@dknapp.com>
2017-08-06 18:05:40 -0400

    Looks like apiKeys.js is working properly now.  A few typeos.

--- apiKeys.js
+++ apiKeys.js
@@ -1,5 +1,5 @@
 'use strict';
-// 
+//
 // This needs to be "objectified" so that config is persistent.
 //
 const apiKeys = {};
@@ -12,13 +12,16 @@ apiKeys.keyFile = undefined;
 apiKeys.keys = undefined;
 
 apiKeys.apiKeyCheck = function ( req, res, next ) {
-    keybug( req.headers );
+    keybug( "Query   : ", req.query );
+    keybug( "Headers : ", req.headers );
+    keybug( "list    : ", apiKeys.keys );
     let apiKey = req.query.apikey;                // Check query first
     if ( ! apiKey ) apiKey = req.headers.apikey;  // Then look in headers
     keybug( "apiKey provided: ", apiKey );
-    if ( ! apiKey ) return res.redirect( apiKey.unauthRedirect );
+    if ( ! apiKey ) return res.redirect( apiKeys.unauthRedirect );
     const entry = apiKeys.keys.find( e => { return e.key === apiKey } );
-    if ( ! entry || ! entry.user ) return res.redirect( apiKey.unauthRedirect );
+    keybug( "Found   : ", entry );
+    if ( ! entry || ! entry.user ) return res.redirect( apiKeys.unauthRedirect );
     if ( !req.body ) req.body = {};
     req.body.user = entry;
     next();
